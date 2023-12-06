1db9449614c3500026ba86cd0a7443cd6a741112
JonathanPena17 <jonathanrpena23@gmail.com>
2023-12-05 22:53:51 -0800

    Finished Parser

--- civilParser.py
+++ civilParser.py
@@ -9,7 +9,7 @@ html_collection = db["civilProfs"]
 research_collection = db["civilResearch"]
 
 # Function to parse and persist professor's information
-def parse_and_persist_professor_info(html):
+def parse_and_persist_professor_info(html,url):
     # Use BeautifulSoup to parse the HTML
     soup = BeautifulSoup(html, 'html.parser')
 
@@ -21,50 +21,49 @@ def parse_and_persist_professor_info(html):
     office_location = soup.find('p', class_='locationicon').find('a')['href']
     office_hours = [item.text.strip() for item in soup.find('p', class_='hoursicon').find_all('span')]
 
+    # Create an array to store the area of search for each blurb
+    area_of_search = []
+
+    # Iterate through blurb divs and categorize based on headers
+    blurb_divs = soup.find_all('div', class_='blurb')
+    for blurb in blurb_divs:
+        header = blurb.find('h2').text.strip()  # Extract the header text and strip whitespace
+        content = blurb.get_text().strip()  # Get the content of the blurb and strip leading/trailing whitespace
+        
+        # Store the content in the list with the header as the key
+        area_of_search.append({header: content})
+
+
     # Extract Research Interests
     research_interests = []
 
-    try:
-        accolades_divs = soup.find_all('div', class_='accolades')
-
-        # Initialize variables to store the divs containing research interests
-        research_interests_tag = None
-
-        # Iterate through the accolades divs
-        for div in accolades_divs:
-            h2_tag = div.find('h2')
-            if h2_tag and ("Research Interests" in h2_tag.text or "Research Interest" in h2_tag.text):
-                research_interests_tag = div
-                break  # Exit the loop when the correct div is found
-
-        if research_interests_tag:
-                    # Check if the structure is a simple list using <ul> and <li> tags
-                    ul_tag = research_interests_tag.find_next('ul')
-                    if ul_tag:
-                        research_interests = [item.text.strip() for item in ul_tag.find_all('li')]
-                    else:
-                        # Check if the structure is a simple list using <div> and <p> tags
-                        p_tags = research_interests_tag.find_all('p')
-                        if p_tags:
-                                research_interests = [p.text.strip() for p in p_tags if p.text.strip()]
-                        
-    except Exception as e:
-        print(f"Error extracting research interests: {e}")
+    # Iterate through accolades divs and categorize based on headers
+    accolades_divs = soup.find_all('div', class_='accolades')
+    for accolades in accolades_divs:
+        header = accolades.find('h2').text.strip()  # Extract the header text and strip whitespace
+        content = accolades.get_text().strip()  # Get the content of the accolades and strip leading/trailing whitespace
+        
+        # Store the content in the list with the header as the key
+        research_interests.append({header: content})
 
     # Insert the professor's information into the research collection
-    if name and title_dept and email and phone and office_location and office_hours and research_interests:
+    if name and title_dept and email and phone and office_location and office_hours and research_interests and area_of_search :
         professor_info = {
             "name": name,
+            "url": url,
             "title_dept": title_dept,
             "email": email,
             "phone": phone,
             "office_location": office_location,
             "office_hours": office_hours,
-            "research_interests": research_interests
+            "research_interests": research_interests,
+            "area_of_search": area_of_search,
+
         }
         research_collection.insert_one(professor_info)
     else:
         print(f"Skipping professor {name} due to missing information.")
+    
 
 
 # Retrieve HTMLs from MongoDB
@@ -73,7 +72,8 @@ htmls_of_professor_pages = html_collection.find()
 # Iterate through the HTMLs of professor web pages
 for html_doc in htmls_of_professor_pages:
     html = html_doc['html']  # Assuming 'html' is the key for HTML content in the MongoDB document
-    parse_and_persist_professor_info(html)
+    url  = html_doc['url'] 
+    parse_and_persist_professor_info(html,url)
 
 # Print a message when parsing is done
 print("Parsing and persisting professor information is done.")
