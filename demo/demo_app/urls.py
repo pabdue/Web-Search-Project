0a9a09c68b84d834d71c8a9b7730e9e1d4444c6f
Pablo Dueñas <pabloduenas@Pablos-MBP.home>
2023-12-07 18:56:29 -0800

    update demo UI, readme, and model

--- demo/demo_app/urls.py
+++ demo/demo_app/urls.py
@@ -7,5 +7,6 @@ app_name = 'demo_app'
 
 urlpatterns = [
     path('demo/search/', views.search_view, name='search_view'),
+    path('demo/about/', views.about_view, name='about_view'),
     path('search/', views.search_civil_research, name='search_civil_research'),
 ]
--- demo/demo_app/views.py
+++ demo/demo_app/views.py
@@ -5,6 +5,9 @@ from .models import CivilResearch
 def search_view(request):
     return render(request, 'search.html')
 
+def about_view(request):
+    return render(request, 'about.html')
+
 def search_civil_research(request):
     query = request.GET.get('query', '')
 
